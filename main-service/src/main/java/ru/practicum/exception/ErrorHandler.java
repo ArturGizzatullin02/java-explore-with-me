9103a5fc0a9ed0e5513e2d01849e01a2b3087e96
Artur Gizzatullin <salatikufa@yandex.ru>
2025-02-04 19:04:35 +0500

    Написан ErrorHandler и описана сущность ошибки

---
+++ main-service/src/main/java/ru/practicum/exception/ErrorHandler.java
@@ -0,0 +1,24 @@
+package ru.practicum.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+@Slf4j
+public class ErrorHandler {
+
+    public ErrorHandler() {
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ApiError handleException(Exception e) {
+        log.warn(e.getMessage(), e);
+        return new ApiError(e.getMessage(), e.getLocalizedMessage(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), LocalDateTime.now().toString());
+    }
+}
