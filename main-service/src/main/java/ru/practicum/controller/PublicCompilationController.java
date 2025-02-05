a336be0ded3c425e8d3cbf2a72f72c45365a8ea8
Artur Gizzatullin <salatikufa@yandex.ru>
2025-02-05 13:26:13 +0500

    Написан контроллер для подборок событий

---
+++ main-service/src/main/java/ru/practicum/controller/PublicCompilationController.java
@@ -0,0 +1,37 @@
+package ru.practicum.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.dto.CompilationDto;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class PublicCompilationController {
+
+    private final CompilationService compilationService;
+
+    @GetMapping("/compilations")
+    public List<CompilationDto> getCompilations(@RequestParam Boolean pinned,
+                                                @RequestParam(defaultValue = "0") int from,
+                                                @RequestParam(defaultValue = "10") int size) {
+        log.info("getCompilations for {} {} started", pinned, size);
+        List<CompilationDto> compilationDtos = compilationService.getCompilations(pinned, from, size);
+        log.info("getCompilations for {} {} finished", pinned, compilationDtos);
+        return compilationDtos;
+    }
+
+    @GetMapping("/compilations/{compId}")
+    public CompilationDto getCompilation(@PathVariable long compId) {
+        log.info("getCompilation for {} started", compId);
+        CompilationDto compilationDto = compilationService.getCompilation(compId);
+        log.info("getCompilation for {} finished", compilationDto);
+        return compilationDto;
+    }
+}
